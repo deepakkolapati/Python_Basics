abaa7ada0b4e606b83dd31603923e1951f29207c
deepakkolapati <deepakchandu9@gmail.com>
2024-01-19 15:12:42 +0530

    [deepak] Added list datastructures module

--- lists_ds.py
+++ lists_ds.py
@@ -41,17 +41,39 @@ def count_first_and_last(list,len):
 def clone(list):
     return list.copy()
 
-def remove_duplicate(list):
+def remove_duplicates(list):
     new_list=[]
     for i in list:
         if i not in new_list:
             new_list.append(i)
     return new_list
 
+def count_longer(list,n):
+    count=0
+    for str in list:
+        if len(str)>n:
+            count+=1
+    return count
 
-
+def one_common(list1,list2):
+    for i in list1:
+        if i in list2:
+            return True
+    return False
+def append(list1,list2):
+    list3=list1
+    list3.extend(list2)
+    return list3
 
 
 if __name__ == "__main__":
-    print(sum([1,2,3,4,5]))
-
+    list=[1,2,3,2,1,4,5,6,4,3,2]
+    print(sum(list))
+    print(mul(list))
+    print(min(list))
+    print(clone(list))
+    print(remove_duplicates(list))
+    newlist=["apple","banana","guava","lemon","orange","water melon"]
+    print(count_longer(newlist,6))
+    print(one_common([1,2,3,4,5],[6,7,7,8,2]))
+    print(append([1,2,3,4,5],[6,7,7,8,2]))
