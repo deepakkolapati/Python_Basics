73d86a19e02d6ca8f0af20fab8ff53c83be083ab
deepakkolapati <deepakchandu9@gmail.com>
2024-01-20 13:40:29 +0530

    [deepak] Added . UC5 of Employee Wage Problem

--- employee_wage_problem/employee_wage_problem.py
+++ employee_wage_problem/employee_wage_problem.py
@@ -23,7 +23,7 @@ def check_attendance() -> int:
     is_present =random.randint(0,2)
     return is_present
 
-def caluclate_wage(wage_per_hour=8,full_day_hour=8,part_time_hour=4) -> int:
+def caluclate_daily_wage(wage_per_hour=8,full_day_hour=8,part_time_hour=4) -> int:
     """ Caluclates the wage of the employee per day
     
     params: wage_per_hour,full_day_hour,part_time_hour
@@ -38,13 +38,21 @@ def caluclate_wage(wage_per_hour=8,full_day_hour=8,part_time_hour=4) -> int:
         case 0:
             return 0
     
-def calculate_monthly_wage():
-
-    pass
+def calculate_monthly_wage(no_of_days=20) -> int:
+    """ Caluclates the wage of the employee per month
+    
+    params: no_of_days
+    Return: int
+    """
+    
+    monthly_wage=0
+    for i in range(no_of_days):
+        monthly_wage+=caluclate_daily_wage
+    return monthly_wage
 
 def main():
     print("Welcome to Employee Wage problem")
-    print(f"The employees wage is {caluclate_wage()}")
+    print(f"The employees wage is {caluclate_daily_wage()}")
 
 
 if __name__=="__main__":
