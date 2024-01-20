9c3fced9a306ed5333050f9504ceed44f767dd29
deepakkolapati <deepakchandu9@gmail.com>
2024-01-20 12:12:06 +0530

    [deepak] Added . UC2 of Employee Wage problem

--- employee_wage_problem/employee_wage_problem.py
+++ employee_wage_problem/employee_wage_problem.py
@@ -23,10 +23,22 @@ def check_attendance() -> int:
     is_present = random.randint(0,1)
     return True if is_present == 1 else False
 
- 
+def caluclate_wage(wage_per_hour=8,full_day_hour=8) -> int:
+    """ Caluclates the wage of the employee per day
+    
+    params: wage_per_hour,full_day_hour,is_present
+    Return: int
+    """
+    is_present=check_attendance()
+    if is_present:
+       return wage_per_hour*full_day_hour
+    
+    return 0
+    
+
 def main():
     print("Welcome to Employee Wage problem")
-    print(check_attendance())
+    print(f"The employees wage is {caluclate_wage()}")
 
 
 if __name__=="__main__":
