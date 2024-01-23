45df0355a26f854d412334679557818a5e207be7
deepakkolapati <deepakchandu9@gmail.com>
2024-01-23 14:20:06 +0530

    [deepak] Refactored Uc7 of snake and ladder game using game class and player class

--- snakeladder/snakeladder.py
+++ snakeladder/snakeladder.py
@@ -11,53 +11,58 @@
 @Title : Creating Snake Ladder Program
 
 '''
+
+
 import random
+class Player:
+    def __init__(self):
+        self.position=0
+    def roll_dice(self):
+        dice=random.randint(1,6)
+        options=["snake","ladder","no play"]
+        extra_chance=True
+        while extra_chance and self.position!=100:
+            option=random.choice(options)
+            if option=="snake":
+                self.position-=dice
+                if self.position<0:
+                    self.position=0
+                extra_chance=False
+                
+            elif option=="ladder":
+                if self.position+dice<=100:
+                    self.position+=dice
+            else:
+                extra_chance=False
 
 
+class Game:
+    def __init__(self,player1,player2):
+        self.player1=player1
+        self.player2=player2
+    def start_game(self):
+        print("Welcome to the Snake and Ladder Program")
+        player=0
+        dice_count=0
+        while self.player1.position!=100 and self.player2.position!=100:
+            if player==0:
+                self.player1.roll_dice()
+            if player==1:
+                self.player2.roll_dice()
+            dice_count+=1
+            if self.player1.position==100:
+                print("Player 1 won the game")
+                break
+            if self.player2.position==100:
+                print("Player 2 won the game")
+                break
+            player=(player+1)%2
+        print(f"The dice rolled for {dice_count} times")
 
-def roll_dice(player_position):
-    dice=random.randint(1,6)
-    options=["snake","ladder","no play"]
-    extra_chance=True
-    while extra_chance and player_position!=100:
-        option=random.choice(options)
-        if option=="snake":
-            player_position-=dice
-            if player_position<0:
-                player_position=0
-            extra_chance=False
-            
-        elif option=="ladder":
-            if player_position+dice<=100:
-                player_position+=dice
-        else:
-            extra_chance=False
-
-        
-    return player_position
 
 
 if __name__=="__main__":
-    print("Welcome to the Snake and Ladder Program")
-    player1_position=0
-    player2_position=0
-    player=0
-    dice_count=0
-    while player1_position!=100 and player2_position!=100:
-        if player==0:
-            player1_position=roll_dice(player1_position)
-        if player==1:
-            player2_position=roll_dice(player2_position)
-        dice_count+=1
-        if player1_position==100:
-            print("Player 1 won the game")
-            break
-        if player2_position==100:
-            print("Player 2 won the game")
-            break
-        player=(player+1)%2
-    print(f"The dice rolled for {dice_count} times")
-
-
-
-
+    p1=Player()
+    p2=Player()
+    snake_and_Ladder=Game(p1,p2)
+    snake_and_Ladder.start_game()
