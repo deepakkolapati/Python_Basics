8e67e4b7e3cb619b619acb9b5552ac1c527587d6
deepakkolapati <deepakchandu9@gmail.com>
2024-01-23 11:41:55 +0530

    [deepak] Added . UC3 of SnakeLadder Program

--- snakeladder/snakeladder.py
+++ snakeladder/snakeladder.py
@@ -17,3 +17,12 @@ player_position=0
 
 def roll_dice():
     return random.randint(1,6)
+
+def check_option():
+    options=["snake","ladder","no play"]
+    option=random.choice(options)
+    return option
+
+if __name__=="__main__":
+    print(roll_dice())
+    print(check_option())
