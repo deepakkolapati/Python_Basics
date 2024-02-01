60fd8faa04af855515f228ebf64266edbd09a89f
deepakkolapati <deepakchandu9@gmail.com>
2024-02-01 11:01:42 +0530

    [deepak] Refactored Stack Class

--- stack_class.py
+++ stack_class.py
@@ -12,35 +12,40 @@
 
 '''
 
+from linkedlistclass import LinkedList
 
-from linkedlistclass import Node,LinkedList
 
 class Stack:
     def __init__(self):
-        self.stack=LinkedList()
+        self.stack = LinkedList()
 
-    def push(self,val):
+    def push(self, val):
         self.stack.push(val)
 
     def pop(self):
-        val=self.stack.pop()
+        val = self.stack.pop()
         return val
 
-    def isempty(self):
+    def is_empty(self):
         return self.stack.isempty()
 
     def length(self):
         return self.stack.length
 
-    def print(self):
+    def show(self):
         self.stack.print()
-if __name__=="__main__":
-    s=Stack()
+
+
+if __name__ == "__main__":
+    s = Stack()
     s.push(10)
     s.push(20)
     s.push(30)
     s.push(40)
-    s.print()
+    s.show()
     s.pop()
     s.pop()
-    s.print()
+    s.show()
+    s.pop()
+    s.pop()
+    s.show()
